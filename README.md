75c85c2d2ec6e732d818ca04d84949ecc37cf0fd
Fabrice N'DRI <nyf005@gmail.com>
2022-07-15 21:54:50 +0000

    Edit README

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # restaurant-project
+
+## Project description
+
+A restaurant page using Javascript to generate entire contents of the website
+
+### Built With
+
+* Webpack
+
+## Links
+
+[Try demo here!](https://nyf005.github.io/sign-up-form/)
+[Link to the Assignment](https://www.theodinproject.com/lessons/node-path-javascript-restaurant-page)
+
+## Screenshots
+
+![Home Screenshot](https://user-images.githubusercontent.com/32721495/179315918-5cebe97f-8051-4ea9-9e63-3719da6c4338.png)
+![Menus Screenshot](https://user-images.githubusercontent.com/32721495/179315923-febeae1c-726f-44ae-a4e4-072ab0b08b67.png)
+![Contact Screenshot](https://user-images.githubusercontent.com/32721495/179315928-42ddb134-aad1-4fe3-bcc8-f2d3124744d4.png)
--- src/index.js
+++ src/index.js
@@ -5,47 +5,45 @@ import createHome from "./section/home";
 import createOurMenu from "./section/our-menu";
 import createContact from "./section/contact";
 
-const controller = (() => {
-  loadPage();
+loadPage();
 
-  const content = document.getElementById("content");
-  const menuItems = document.querySelectorAll(".menu-item");
-  const ourMenuBtn = document.getElementById("menuBtn");
-  const getInTouchBtn = document.getElementById("contactBtn");
+const content = document.getElementById("content");
+const menuItems = document.querySelectorAll(".menu-item");
+const ourMenuBtn = document.getElementById("menuBtn");
+const getInTouchBtn = document.getElementById("contactBtn");
 
-  function setActiveMenu(newActive) {
-    const currentActive = document.getElementsByClassName("active");
-    if (newActive != currentActive) {
-      currentActive[0].classList.toggle("active");
-      newActive.classList.toggle("active");
-    }
+function setActiveMenu(newActive) {
+  const currentActive = document.getElementsByClassName("active");
+  if (newActive != currentActive) {
+    currentActive[0].classList.toggle("active");
+    newActive.classList.toggle("active");
   }
+}
 
-  menuItems.forEach((menuItem) => {
-    menuItem.addEventListener("click", (e) => {
-      setActiveMenu(e.target);
-      switch (e.target.textContent) {
-        case "OUR MENU":
-          content.replaceChild(createOurMenu(), content.childNodes[1]);
-          break;
+menuItems.forEach((menuItem) => {
+  menuItem.addEventListener("click", (e) => {
+    setActiveMenu(e.target);
+    switch (e.target.textContent) {
+      case "OUR MENU":
+        content.replaceChild(createOurMenu(), content.childNodes[1]);
+        break;
 
-        case "CONTACT":
-          content.replaceChild(createContact(), content.childNodes[1]);
-          break;
+      case "CONTACT":
+        content.replaceChild(createContact(), content.childNodes[1]);
+        break;
 
-        default:
-          content.replaceChild(createHome(), content.childNodes[1]);
-      }
-    });
+      default:
+        content.replaceChild(createHome(), content.childNodes[1]);
+    }
   });
+});
 
-  ourMenuBtn.addEventListener("click", () => {
-    setActiveMenu(menuItems[1]);
-    content.replaceChild(createOurMenu(), content.childNodes[1]);
-  });
+ourMenuBtn.addEventListener("click", () => {
+  setActiveMenu(menuItems[1]);
+  content.replaceChild(createOurMenu(), content.childNodes[1]);
+});
 
-  getInTouchBtn.addEventListener("click", () => {
-    setActiveMenu(menuItems[2]);
-    content.replaceChild(createContact(), content.childNodes[1]);
-  });
-})();
+getInTouchBtn.addEventListener("click", () => {
+  setActiveMenu(menuItems[2]);
+  content.replaceChild(createContact(), content.childNodes[1]);
+});
